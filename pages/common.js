436967e1617660cfb114654209f0abaedfeb39a9
Jonas Wiklund <jwiklund@atex.com>
2010-08-05 16:17:26 +0200

    Added test page

--- pages/common.js
+++ pages/common.js
@@ -1,15 +1,17 @@
-(function ($, page) {
+(function ($, kbmod, page) {
     $("body").keydown(function(event){
 	    // todo smart handling of space, keyCode 32
 	    var link = null
-	    if (event.keyCode == 39) {
+	    if (event.keyCode == 39) {        // <- 
 		link = page.next()
-	    } else if (event.keyCode == 37) {
+	    } else if (event.keyCode == 37) { // ->
 		link = page.prev()
+	    } else if (event.keyCode == 84) { // t
+		kbmod.verify(page)
 	    }
 	    if (link) {
 		window.location = link
 		event.preventDefault()
 	    }
 	})
-})(jQuery, currentPageEngine)
+})(jQuery, lib_kbmod, currentPageEngine)
